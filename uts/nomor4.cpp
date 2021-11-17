ab9a84dcbddd61609009275a7eb43b368d750f9a
root <root@LAPTOP-4O9OBULV.localdomain>
2021-11-17 21:29:59 +0700

    uts no5

---
+++ uts/nomor4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int umur,tahun;
+    char nama[50];
+
+    cout<<"Masukkan Nama Anda : ";cin>>nama;
+    cout<<"Masukkan Tahun Lahir Anda : ";cin>>tahun;
+
+    umur=2021-tahun;
+
+    cout<<"Berarti usia "<<nama<<" sekarang adalah "<<umur<<" tahun ";
+
+return 0;
+}
---
+++ uts/nomor5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+ 
+    int angka1, angka2;
+ 
+    cout <<"Masukan angka pertama : ";
+    cin>>angka1;
+    cout <<"Masukan angka kedua : ";
+    cin>> angka2;
+ 
+    cout<<"penjumlahan = "<<angka1+angka2<<endl;
+    cout<<"pengurangan = "<<angka1-angka2<<endl;
+    cout<<"perkalian = "<<angka1*angka2<<endl;
+    cout<<"pembagian = "<<angka1/angka2<<endl;
+
+    return 0;
+}
