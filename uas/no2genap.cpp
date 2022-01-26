f9affec0bc8c55a46ed23dd4205a43bf5cc41655
root <root@LAPTOP-4O9OBULV.localdomain>
2022-01-26 22:57:14 +0700

    no2genap

---
+++ uas/no2genap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void fname(){
+    cout << "Joseph";
+}
+
+void mname(){
+    cout << "Febrian";
+}
+
+void lname(){
+    cout << " (20200801208)";
+}
+
+void spasi(){
+    cout<<" ";
+}
+
+int main() {
+    fname();
+    spasi();
+    mname();
+    spasi();
+    lname();
+    return 0;
+}
